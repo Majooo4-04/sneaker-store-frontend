2e26abec6b6698d7a5d7b0e102288a340bcbe4e2
Majooo4F <majoflores734@gmail.com>
2026-08-07 19:11:41 -0600

    Conectar frontend con backend de Render

--- src/services/api.js
+++ src/services/api.js
@@ -1,44 +1,70 @@
+// import axios from "axios";
+
+
+// const api = axios.create({
+
+//     baseURL:"http://localhost:3001/api"
+
+// });
+
+
+// // Agregar token automáticamente
+
+// api.interceptors.request.use(
+    
+//     config=>{
+
+
+//         const token = localStorage.getItem("token");
+
+
+//         if(token){
+
+//             config.headers.Authorization =
+//             `Bearer ${token}`;
+
+//         }
+
+
+//         return config;
+
+//     },
+
+
+//     error=>{
+
+//         return Promise.reject(error);
+
+//     }
+
+// );
+
+
+
+// export default api;
+
+
 import axios from "axios";
 
-
 const api = axios.create({
-
-    baseURL:"http://localhost:3001/api"
-
+    baseURL: import.meta.env.VITE_API_URL
 });
 
-
 // Agregar token automáticamente
-
 api.interceptors.request.use(
-    
-    config=>{
-
-
+    (config) => {
         const token = localStorage.getItem("token");
 
-
-        if(token){
-
-            config.headers.Authorization =
-            `Bearer ${token}`;
-
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
         }
 
-
         return config;
-
     },
 
-
-    error=>{
-
+    (error) => {
         return Promise.reject(error);
-
     }
-
 );
-
-
 
 export default api;
